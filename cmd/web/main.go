81faaec547fba738535a84a778e5e30bd47992d7
Артур Низамитдинов <anezamus-10@mail.ru>
2025-01-23 17:04:02 +0300

    Add static files

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -11,6 +11,12 @@ func main() {
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
+	// Создаем обработчик статических файлов, которые есть в ./ui/static
+	fileServer := http.FileServer(http.Dir("./ui/static"))
+	// Переходя на URL /static/css/main.css мы должны убрать префикс /static чтобы
+	// было не -> ./ui/static/static/css/main.css <- А БЫЛО -> ./ui/static/css/main.css <-
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	log.Println("Server is listening on http://127.0.0.1:4000")
 	log.Fatal(http.ListenAndServe(":4000", mux))
 }
