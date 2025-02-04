cb0dd6d4d121f4e28e248c3583d9244bebfa191e
Артур Низамитдинов <anezamus-10@mail.ru>
2025-02-04 14:52:33 +0300

    Add helpers

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -12,7 +12,7 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	// Путь "/" - многоуровневый. Поэтому home обрабатывает любой путь.
 	// Чтобы такого не было добавим проверку if.
 	if r.URL.Path != "/" {
-		http.NotFound(w, r)
+		app.notFound(w)
 		return
 	}
 
@@ -24,14 +24,12 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		app.errorLog.Println(err.Error())
-		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		app.serverError(w, err)
 		return
 	}
 
 	if err = ts.Execute(w, nil); err != nil {
-		app.errorLog.Println(err.Error())
-		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		app.serverError(w, err)
 	}
 }
 
@@ -39,7 +37,7 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
-		http.NotFound(w, r)
+		app.notFound(w)
 		return
 	}
 
@@ -51,8 +49,7 @@ func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 	// Все методы кроме POST надо запретить и вызвать ошибку 405 (метод запрещён)
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		w.Write([]byte("Разрешен только POST-метод\n"))
+		app.clientError(w, http.StatusMethodNotAllowed)
 		return
 	}
 
---
+++ cmd/web/helpers.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"runtime/debug"
+)
+
+
+// serverError записывает сообщение об ошибке в errorLog и затем
+// отправляет пользователю ответ 500 "Внутренняя ошибка сервера"
+func (app *application) serverError(w http.ResponseWriter, err error) {
+	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
+	app.errorLog.Output(2, trace)
+
+	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+}
+
+// clientError отправляет определенный код состояния и соответствующее описание пользователю.
+func (app *application) clientError(w http.ResponseWriter, status int) {
+	http.Error(w, http.StatusText(status), status)
+}
+
+// notFound это оболочка для clientError, которая отправляет пользователю "404 страница не найдена"
+func (app *application) notFound(w http.ResponseWriter) {
+	app.clientError(w, http.StatusNotFound)
+}
