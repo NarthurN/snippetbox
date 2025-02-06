f83b10f4c961eb2fa26770634b7f309b18ed4f57
Артур Низамитдинов <anezamus-10@mail.ru>
2025-02-06 17:47:27 +0300

    Add Get snippet by id

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"html/template"
 	"net/http"
 	"strconv"
+
+	"github.com/NarthurN/snippetbox/pkg/models"
 )
 
 // home - обработчик главной страницы "/"
@@ -41,7 +44,17 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Fprintf(w, "Отображение заметки с ID %d ...\n", id)
+	s, err := app.snippets.Get(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			app.notFound(w)
+		} else {
+			app.serverError(w, err)
+		}
+		return
+	}
+
+	fmt.Fprintf(w, "%v", s)
 }
 
 // createSnippet - создание заметки "/snippet/create"
